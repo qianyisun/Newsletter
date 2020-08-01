88ce1b8a3d8c54325a6a92a8a09a5848c652e2e9
John Qianyi Sun <54040058+qianyisun@users.noreply.github.com>
2020-07-31 20:32:15 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,16 +8,16 @@ const https = require("https");
 const app = express();
 
 
-app.use(express.static("public"));//把本地的文件弄到网页上
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req,res){
-  res.sendFile(__dirname + "/signup.html");//html能够work因为它是绝对URL
+  res.sendFile(__dirname + "/signup.html");
 });
 
 app.post("/", function(req,res){
 
-  const firstName = req.body.fName;//CONST不会去改变firstName这些variable。这样更加保险当variable你不会去改变的时候
+  const firstName = req.body.fName;
   const lastName = req.body.lName;
   const email = req.body.email;
 
@@ -57,12 +57,12 @@ const request = https.request(url,options,function(response){
   })
 })
 
-// request.write(jsonData);
+
 request.end();
 
 
 });
-//导引去home page如果失败的话
+
 app.post("/failure",function(req,res){
   res.redirect("/");
 })
@@ -72,8 +72,4 @@ app.listen(process.env.PORT || 3000, function(){
   console.log("Server is running on port 3000");
 });
 
-//API KEY
-//691abc9e035458ab86a635fcb1d5b99f-us10
 
-//LIST ID
-// 91ce71a0b2
